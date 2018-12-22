15f008611bee20274d688f0ebd180d64f018d343
thivernale <rainm000n@gmail.com>
2018-12-22 17:12:09 +0200

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -23,5 +23,31 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
+    },
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // filter by show completed
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // filter by search text
+        filteredTodos = filteredTodos.filter((todo) => {
+            return searchText.length === 0 || todo.text.toLowerCase().indexOf(searchText) !== -1;
+        });
+
+        // sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -3,7 +3,7 @@ var expect = require('expect');
 var TodoAPI = require('TodoAPI');
 
 describe('TodoAPI', () => {
-    // use a test lifecycle method to clean up local storage before each test
+    // use a test lifecycle method to clean up localStorage before each test
     beforeEach(() => {
         localStorage.removeItem('todos');
     });
@@ -54,6 +54,50 @@ describe('TodoAPI', () => {
 
             var actualTodos = TodoAPI.getTodos();
             expect(actualTodos).toEqual(todos);
+        });
+    });
+
+    describe('filterTodos', () => {
+        // set up data to use for all tests of method
+        var todos = [{
+            id: 1,
+            text: 'Some text here',
+            completed: true
+        },
+        {
+            id: 2,
+            text: 'Other text here',
+            completed: false
+        },
+        {
+            id: 3,
+            text: 'Some text here',
+            completed: true
+        }];
+
+        it('should return all items if showCompleted is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should only return non-completed items if showCompleted is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by searchText', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filteredTodos.length).toBe(2);
+        });
+
+        it('should return all todos when searchText is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
         });
     });
 });
