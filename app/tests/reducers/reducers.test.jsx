664f5054064a139d38f9185acfbf83104f8bb7a5
thivernale <rainm000n@gmail.com>
2019-03-18 00:04:41 +0200

    Wipe todos on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -90,6 +90,21 @@ describe('Reducers', () => {
             expect(res.length).toEqual(1);
             expect(res[0]).toEqual(todos[0]);
         });
+
+        it('should remove todos from store on LOGOUT', () => {
+            var todos = [{
+                id: 111,
+                text: 'any',
+                completed: false,
+                completedAt: undefined,
+                createdAt: 33000
+            }];
+            var action = {
+                type: 'LOGOUT'
+            };
+            var res = reducers.todosReducer(df(todos), df(action));
+            expect(res.length).toEqual(0);
+        });
     });
 
     describe('authReducer', () => {
